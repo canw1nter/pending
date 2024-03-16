c74af23b373f108502df6397ffc5ea74bb774cbf
canwinter <824131148@qq.com>
2024-03-16 12:36:58 +0800

    feat(im_client) add group client for test

--- im/test/chat_server_test.go
+++ im/test/chat_server_test.go
@@ -1,6 +1,7 @@
-package handler
+package test
 
 import (
+	"log"
 	"net/http"
 	"strconv"
 	"testing"
@@ -9,13 +10,17 @@ import (
 )
 
 func TestServerResourceConsume(t *testing.T) {
-	for i := 0; i < 50000; i++ {
+	for i := 0; i < 10000; i++ {
+		i := i
 		go func() {
 			name := "client" + strconv.Itoa(i)
 			dialer := &websocket.Dialer{}
 			var header = http.Header{}
 			header.Add("ID", name)
-			conn, _, _ := dialer.Dial("ws://localhost:8001/online", header)
+			conn, _, err := dialer.Dial("ws://localhost:8001/online", header)
+			if err != nil {
+				log.Printf("%d: %s connect to server failed! err: %s\n", i, name, err.Error())
+			}
 			defer conn.Close()
 
 			var forever chan struct{}
